9f2f2dca801bb1fef2089b61f978c999dd3738c5
SaikatAhmed78 <shaikatahmed78@gmail.com>
2025-08-05 00:36:38 +0600

    ✨ Initial Tailwind CSS setup with custom styles and config
    - Installed Tailwind CSS and integrated with project
    - Structured global styles in index.css
    - Customized tailwind.config.js for scalable theming

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl-padding-1 wide:pading-r padding-b">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Supar Quality</section>
+      <section className="padding padding-x py-10">Services</section>
+      <section className="padding">Special Offers</section>
+      <section className="bg-pale-blue padding">Customer Reviews</section>
+      <section className="padding-x sm-py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
+  );
+};
+
+export default App;
