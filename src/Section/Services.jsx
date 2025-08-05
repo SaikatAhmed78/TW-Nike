4833405f40130de3b72cafa14155448470214d6b
SaikatAhmed78 <shaikatahmed78@gmail.com>
2025-08-06 01:43:15 +0600

    feat: add Services section with feature cards
    - Created Services component with:
      - Responsive grid of service cards
      - Data-driven from constants
    - Built ServiceCard featuring

--- src/Section/Services.jsx
+++ src/Section/Services.jsx
@@ -1,9 +1,16 @@
+import ServiceCard from "../Components/ServiceCard"
+import { services } from "../Constants"
 
 const Services = () => {
+
   return (
-    <div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+
+      {services.map((service) =>(
+        <ServiceCard key={service.label} {...service} />
+      ))}
       
-    </div>
+    </section>
   )
 }
 
